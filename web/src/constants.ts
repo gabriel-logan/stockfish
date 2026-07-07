908c8b0406d3b36f0d323eff69f6895a3abbdd80
Gabriel Logan <96627244+gabriel-logan@users.noreply.github.com>
2026-07-07 00:57:20 -0300

    feat(web): initial structure ready to start implementation

---
+++ web/src/constants.ts
@@ -0,0 +1,19 @@
+import { MoveClassification } from "./types/enums";
+
+export const CLASSIFICATION_COLORS: Record<
+  (typeof MoveClassification)[keyof typeof MoveClassification],
+  string
+> = {
+  [MoveClassification.Opening]: "#dbac86",
+  [MoveClassification.Forced]: "#dbac86",
+  [MoveClassification.Splendid]: "#19d4af",
+  [MoveClassification.Perfect]: "#3894eb",
+  [MoveClassification.Best]: "#22ac38",
+  [MoveClassification.Excellent]: "#22ac38",
+  [MoveClassification.Okay]: "#74b038",
+  [MoveClassification.Inaccuracy]: "#f2be1f",
+  [MoveClassification.Mistake]: "#e69f00",
+  [MoveClassification.Blunder]: "#df5353",
+};
+
+export const BASE_URL_API = "http://localhost:3000";
---
+++ web/src/types/enums.ts
@@ -0,0 +1,17 @@
+export const MoveClassification = {
+  Blunder: "blunder",
+  Mistake: "mistake",
+  Inaccuracy: "inaccuracy",
+  Okay: "okay",
+  Excellent: "excellent",
+  Best: "best",
+  Forced: "forced",
+  Opening: "opening",
+  Perfect: "perfect",
+  Splendid: "splendid",
+} as const;
+
+export const Color = {
+  White: "w",
+  Black: "b",
+} as const;
